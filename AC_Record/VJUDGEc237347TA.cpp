c3da07eb1f81bbe4f93ea0ed4491b5786d831ffd
chy-2003 <528574785@qq.com>
2018-07-16 16:13:43 +0800

    some message

--- AC_Record/VJUDGEc237347TA.cpp
+++ AC_Record/VJUDGEc237347TA.cpp
@@ -1,84 +1,96 @@
 #include<bits/stdc++.h>
 using namespace std;
-char ch[50000];
-int nxt[50000], casenum, i, j, len, last, deep, cas, in;
-inline void tab(int x){
-	printf("\n");
-	for(int i = 0; i < x; i++) printf(" ");
-	return;
+int casenum, l, nxt[100010], last[100010], in;
+char ch[100010], c;
+void del(int x){
+    nxt[last[x]] = nxt[x];
+    last[nxt[x]] = last[x];
+    return;
+}
+void tab(int x){
+    printf("\n");
+    for(int i = 1; i <= x; i++) printf(" ");
+    return;
 }
 int main(){
-	scanf("%d", &casenum);
-	len = 0; 
-	while(gets(ch + len) != NULL){
-		 if(!ch[0]) continue;
-		 len = strlen(ch);
-		 ch[len] = ' ';
-		 len++;
-	}
-	in = 0;
-	for(i = 0; i < len; i++){
-		if(ch[i] == '<') in = 1;
-		else if(ch[i] == '>') in = 0;
-		else if(!in && ch[i] == '\t') ch[i] = ' ';
-		nxt[i] = i + 1;
-	}
-	last = 0;
-	in = 0;
-	for(i = 1; i < len; i++){
-		if(ch[i] == '<') in = 1;
-		else if(ch[i] == '>') in = 0;
-		if(ch[i] == ' '){
-			if((in == 0 && ch[last] == ' ') || ch[last] == '>') nxt[last] = nxt[i]; else last = i;
-		}
-		else last = i;
-	}
-	for(i = 0; i < len; i = nxt[i]){
-		if(ch[i] == ' ' && ch[nxt[i]] == '<') nxt[last] = nxt[i];
-		last = i;
-	}
-	deep = 0;
-	cas = 2;
-	printf("Case #1:\n");
-	for(i = 0; i < len;){
-		if(ch[i] == '<' && ch[nxt[i]] == '/' && ch[nxt[nxt[i]]] == '>'){
-			if(deep) tab(deep);
-			printf("</>");
-			i = nxt[nxt[nxt[i]]];
-		} else 
-		if(ch[i] == '<' && ch[i + 1] == '/'){
-			deep--;
-			tab(deep);
-			for(j = i; j < len; j = nxt[j]){
-				printf("%c",ch[j]);
-				if(ch[j] == '>') break;
-			}
-			i = nxt[j];
-			if(deep == 0){
-				if(cas > casenum){
-					printf("\n");
-					return 0;
-				}
-				printf("\nCase #%d:\n", cas++);
-			}
-		} else 
-		if(ch[i] == '<' && ch[i + 1] != '/'){
-			if(deep) tab(deep);
-			for(j = i; j < len; j = nxt[j]){
-				printf("%c", ch[j]);
-				if(ch[j] == '>') break; else 
-				if(ch[j] == '/' && ch[j + 1] == '>') deep--;
-			}
-			i = nxt[j];
-			deep++;
-		} else {
-			tab(deep);
-			for(j = i; j < len; j = nxt[j]){
-				if(ch[j] == '<') break;
-				printf("%c", ch[j]);
-			}
-			i = j;
-		}
-	}
-	return 0;
+    scanf("%d", &casenum);
+    while(scanf("%c", &c) == 1){
+        if(c == '\n') c = ' ';
+        l++;
+        ch[l] = c;
+    }
+    for(int i = 1; i <= l; i++){
+        nxt[i] = i + 1;
+        last[i] = i - 1;
+    }
+    nxt[0] = 1;
+    in = 0;
+    for(int i = 1; i <= l; i++){
+        if(ch[i] == '<') in = 1;
+        if(ch[i] == '>') in = 0;
+        if(!in && (ch[i] == '\t')) ch[i] = ' ';
+    }
+    in = 0;
+    for(int i = 1; i <= l; i++){
+        if(ch[i] == '<') in = 1;
+        if(ch[i] == '>') in = 0;
+        if(in != 1 && ch[i] == ' '){
+            if(ch[last[i]] == '>') del(i); else
+            if(ch[last[i]] == ' ') del(i);
+        } else
+        if(ch[i] == '<'){
+            if(ch[last[i]] == ' ') del(last[i]);
+        }
+    }
+ //   for(int i = nxt[0]; i <= l; i = nxt[i]) printf("%c", ch[i]); printf("\n");
+    in = 0;
+    printf("Case #1:\n");
+    int ll = nxt[0], deep = 0, cas = 1;
+    while(ll <= l){
+        if(ch[ll] == '<' && ch[nxt[ll]] == '/' && ch[nxt[nxt[ll]]] == '>'){
+            if(deep) tab(deep);
+            printf("</>");
+            ll = nxt[nxt[nxt[ll]]];
+        } else
+        if(ch[ll] == '<' && ch[nxt[ll]] == '/'){
+            deep--;
+            tab(deep);
+            int l1 = ll;
+            while(1){
+                printf("%c", ch[l1]);
+                if(ch[l1] == '>') break;
+                l1 = nxt[l1];
+            }
+            ll = nxt[l1];
+            if(deep == 0){
+                if(cas == casenum){
+                    printf("\n");
+                    return 0;
+                }
+                printf("\nCase #%d:\n", ++cas);
+            }
+        } else
+        if(ch[ll] == '<' && ch[nxt[ll]] != '/'){
+            if(deep) tab(deep);
+            int l1 = ll;
+            while(1){
+                printf("%c", ch[l1]);
+                if(ch[l1] == '>') break;
+                if(ch[l1] == '/' && ch[nxt[l1]] == '>') deep--;
+                l1 = nxt[l1];
+            }
+            deep++;
+            ll = nxt[l1];
+        } else{
+            tab(deep);
+            int l1 = ll;
+            while(1){
+                if(ch[l1] == '<') break;
+                printf("%c", ch[l1]);
+                l1 = nxt[l1];
+            }
+            ll = l1;
+        }
+    }
+    return 0;
 }
